01efae6a2f02d31d61d404dd8a3bf560472965d7
Srdan Srepfler <schrepfler@gmail.com>
2014-12-27 20:30:57 +0100

    changing the code

--- test/wadl2wiki.js
+++ test/wadl2wiki.js
@@ -1,9 +1,9 @@
 var should = require('chai').should(),
-    wadl2wiki = require('../lib/wadl2wiki');
+    wadl2wiki = require('../lib/wadl2wiki.js');
 
 describe('#handlebarsHelpers', function() {
 
-  var helpers = raml2wiki.handlebars.helpers; 
+  var helpers = wadl2wiki.handlebars.helpers; 
   
   it('escapes { ', function() {
     helpers.wikisafe('{').should.equal('\\{');
